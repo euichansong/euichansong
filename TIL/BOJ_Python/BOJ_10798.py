22351c8ea8a459454e8cbab92064f12e6d31a8d2
Euichan Song <zxcasdqwer38@naver.com>
2023-07-25 17:53:23 +0900

    try 10798

---
+++ TIL/BOJ_Python/BOJ_10798.py
@@ -0,0 +1,23 @@
+#a,b,c,d,e = list(input())
+a = list(input())
+b = list(input())
+c = list(input())
+d = list(input())
+e = list(input())
+# # for h in range(5):
+# for i in range(len(a)):
+#     for j in range(len(b)):
+#         for k in range(len(c)):
+#             for l in range(len(d)):
+#                 for m in range(len(e)):
+#                     print(a[i]+b[j]+c[k]+d[l]+e[m], end = ' ')
+#                         #print(a+b+c+d+e, end = ' ')
+
+# print(max(len(a,b,c,d,e)))
+print(a,b,c,d,e)
+
+첫번째 줄의 0번뽑고 2번째 0번.... 없으면 continue 
+# for i in range():
+#     print(a[i]+b[i]+c[i]+d[i]+e[i] ,end = '')
+# for i in range(len(a)):
+#     kj
