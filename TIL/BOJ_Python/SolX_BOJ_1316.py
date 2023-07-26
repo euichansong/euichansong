dbb2338051f0ba3a117196ba0e559e3708265bb4
Euichan Song <zxcasdqwer38@naver.com>
2023-07-26 15:50:57 +0900

    dontsol1316

---
+++ TIL/BOJ_Python/SolX_BOJ_1316.py
@@ -0,0 +1,21 @@
+a = int(input())
+for i in range(a):
+    text = input()
+    count = 0
+    연결된 문자열 끼리 리스트로 나눔 
+    [i] = [i + 1] append 
+    
+    각 앞의 문자열이 다르면 카운트 같으면 카운트 안함
+
+    딕셔너리 만들어서
+    키로 abc 받음
+    a 들어옴 밸류 더함
+    ㅠ 들어옴 밸류 더함
+    ㅁ 들어옴 
+    
+    
+    
+    
+    if text[i]
+    text[i] = text[i+1] 
+    count += 1
